cbbf86470de51f241579fc3946164a8ecf54b15c
황민규 <0beloved0@naver.com>
2023-05-10 09:55:21 +0900

    Update Lv1(2).py

--- code/programmers/Lv1(2).py
+++ code/programmers/Lv1(2).py
@@ -22,3 +22,23 @@ def check_num(n):
     return flag
   
 ########################
+
+
+# 크레인 인형뽑기 
+
+def solution(board, moves):
+    bucket = []
+    answer = []
+    for move in moves:
+        for i in range(len(board)):
+            if board[i][move-1] > 0:
+                bucket.append(board[i][move-1])
+                board[i][move-1] = 0
+                if bucket[-1:] == bucket[-2:-1]:
+                    answer += bucket[-1:]
+                    bucket = bucket[:-2]
+                break
+    return len(answer)*2
+
+
+########################
