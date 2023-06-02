950751e87d4c90c7378163a3594e8fccf3265951
황민규 <0beloved0@naver.com>
2023-06-02 09:55:15 +0900

    Update Lv2.py

--- code/programmers/Lv2.py
+++ code/programmers/Lv2.py
@@ -256,5 +256,23 @@ def solution(n):
 ##################################################
 
 
+# N개의 최소 공배수 
+
+def gcd(a, b):
+    if a % b == 0:
+        return b 
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+def solution(arr):
+    answer = arr[0]
+    for n in arr:
+        answer = lcm(n , answer)
+    return answer
+
+
+#######################################333
 
 
